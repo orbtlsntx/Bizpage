2319226956e6e5ffd4ef73e1a29f16f515176f15
orbtlsntx <orbitalsyntax@gmail.com>
2021-10-23 22:05:25 -0400

    update

--- pages/script.js
+++ pages/script.js
@@ -3,4 +3,7 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
-})
+   
+    })
+
+
